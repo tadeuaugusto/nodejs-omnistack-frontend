11aed4135eaaf293573e3ad773020c6dfcab1feb
Tadeu Augusto Dutra Pinto <tadeuaugusto@gmail.com>
2019-04-11 15:05:13 -0300

    Creates Box component

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -1,32 +1,50 @@
 import React, { Component } from 'react';
+import api from '../../services/api';
 
 import { MdInsertDriveFile } from 'react-icons/md';
 import logo from '../../assets/logo.svg';
 import './styles.css';
 
 export default class Box extends Component {
+
+  state = {
+    box: {}
+  }
+
+  async componentDidMount() {
+    const boxId = this.props.match.params.id;
+    const response = await api.get(`boxes/${boxId}`);
+
+    this.setState({ box: response.data });
+  }
+
   render() {
     return (
       <div id="box-container">
         <header>
           <img src={ logo } alt="" />
-          <h1>Rocketseat</h1>
+          <h1>{ this.state.box.title }</h1>
         </header>
         <ul>
-          <li>
-            <a href="">
-              <MdInsertDriveFile size={24} color="#A5CFFF" />
-              <strong>Everblast (Earthling & Chromatone) LIVE @ Shiva Valley, Anjuna Goa 2019</strong>
-            </a>
-            <span>3 minutes ago</span>
-          </li>
-          <li>
-            <a href="">
-              <MdInsertDriveFile size={24} color="#A5CFFF" />
-              <strong>DigiCult Psy-Fi Compilation 2019</strong>
-            </a>
-            <span>30 minutes ago</span>
-          </li>
+          { this.state.box.files && this.state.box.files.map(file => (
+            <li>
+              <a className="fileInfo" href={ file.url } target="_blank">
+                <MdInsertDriveFile size={24} color="#A5CFFF" />
+                <strong>{ file.title }</strong>
+              </a>
+              <span>{ file.createdAt }</span>
+            </li>
+            /*
+            <li>
+              <a className="fileInfo" href="">
+                <MdInsertDriveFile size={24} color="#A5CFFF" />
+                <strong>DigiCult Psy-Fi Compilation 2019</strong>
+              </a>
+              <span>30 minutes ago</span>
+            </li>
+            */
+          )) }
+          
         </ul>
       </div>
     )
